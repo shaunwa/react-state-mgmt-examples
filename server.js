a598c322a345b912dc867e54e900b47a16f33612
Shaun Wassell <shaun@trove.ai>
2021-05-19 13:00:51 -0400

    Implemented Full-stack useState example, added template folder, and added body parser to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 let count = 0;
 
--- src/use-state/FullStackExample.js
+++ src/use-state/FullStackExample.js
@@ -1,10 +1,35 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const FullStackExample = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const [numberOfClicks, setNumberOfClicks] = useState(0);
     const [incrementBy, setIncrementBy] = useState(1);
 
-    return (
+    useEffect(() => {
+        const loadCount = async () => {
+            const response = await axios.get('/count');
+            const { count } = response.data;
+            setNumberOfClicks(count);
+            setIsLoading(false);
+        }
+
+        loadCount();
+    }, []);
+
+    const increment = async () => {
+        const response = await axios.put('/count/increment', { amount: incrementBy });
+        const { count } = response.data;
+        setNumberOfClicks(count);
+    }
+
+    const reset = async () => {
+        const response = await axios.put('/count/reset');
+        const { count } = response.data;
+        setNumberOfClicks(count);
+    }
+
+    return isLoading ? <p>Loading...</p> : (
         <div className="counter-container">
             <label>
                 Increment by:
@@ -15,11 +40,11 @@ export const FullStackExample = () => {
             </label>
             <div className="counter-text">You've clicked the button {numberOfClicks} times</div>
             <button
-                onClick={() => setNumberOfClicks(numberOfClicks + incrementBy)}
+                onClick={increment}
                 className="counter-button"
             >Click here</button>
             <button
-                onClick={() => setNumberOfClicks(0)}
+                onClick={reset}
                 className="reset-button"
             >Reset Count</button>
         </div>
